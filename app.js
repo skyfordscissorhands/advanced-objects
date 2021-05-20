752154daab313f48bd465ba6ce6c9b89a8a842a7
skynewell1 <skyfordnewell@gmail.com>
2021-05-20 12:01:23 -0600

    push

---
+++ app.js
@@ -0,0 +1,13 @@
+function Video(title, minutes) {
+
+        this.title= title;
+        this.minutes = minutes;  
+}
+    Video.prototype.watch= function() {
+        alert("You watched all " + this.minutes + " minutes of " + this.title);
+    };
+ 
+    var vid = new Video("Otters Holding Hands", 90);
+    vid.watch();
+    var vid = new Video("Pulp Fiction", 100);
+    vid.watch();
